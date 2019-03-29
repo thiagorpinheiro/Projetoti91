d7b8dd8d51ae7ed3a6166652bf1c447becd9ed3c
THIAGO ROLIM PINHEIRO <thiago.rpinheiro1@senacsp.edu.br>
2019-03-29 12:10:17 -0300

    Initial commit

---
+++ app/src/main/java/sp/senac/br/projetoti91/Menu_Activity.java
@@ -0,0 +1,13 @@
+package sp.senac.br.projetoti91;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Menu_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_layout);
+    }
+}
